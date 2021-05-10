2e6d797bbe31397fea49d2acfa9e2e61b1f46a1b
Aiman Irfan <aimanirfan1017@gmail.com>
2021-05-10 11:35:12 +0800

    Add Procfile, Port number and Change Package.json

--- app.js
+++ app.js
@@ -148,6 +148,8 @@ app.use(function (err, req, res, next) {
   res.status(statusCode).render('error', { err });
 });
 
-app.listen(3000, function () {
-  console.log('Server is starting at port 3000.');
+const port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+  console.log(`Server is starting at port ${port}.`);
 });
