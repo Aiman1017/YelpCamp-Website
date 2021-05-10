1828cadc535881a1a7fbc9221812571e326b26cf
Aiman Irfan <aimanirfan1017@gmail.com>
2021-05-10 12:50:51 +0800

    Fix the Connect-Mongo dependencie

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const MongoDBStore = require('connect-mongo')(session);
 
-const databaseURL = process.env.MONGO_DATABASE || 'mongodb://localhost:27017/yelp-camp';
+//process.env.MONGO_DATABASE ||
+const databaseURL = 'mongodb://localhost:27017/yelp-camp';
 
 mongoose.connect(databaseURL, {
   useNewUrlParser: true,
